2554a85cf37e87b02db3ae137e5e5fdec9892438
Jared J. Peters <jjpeters@dlfna.com>
2018-12-18 14:46:54 -0800

    React.lazy, React.Suspense, dynamic import syntax

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import { debug } from 'util';
-import { hot } from 'react-hot-loader'
+import React from "react";
+import { debug } from "util";
+import { hot } from "react-hot-loader";
+
+const Warning = React.lazy(() => import("./Warning"));
 
 class App extends React.Component {
-    state = {
-        count: 0
-    }
-    render() {
-        return (
-            <div>
-                <h2 className={this.state.count > 10 ? 'warning' : null}>
-                    Numbers: {this.state.count}
-                </h2>
-                <h1>Hello World!</h1>
-                <button onClick={() => this.setState(state => ({ count: state.count + 1 }))}>+</button>
-                <button onClick={() => this.setState(state => ({ count: state.count - 1 }))}>-</button>
-            </div>
-        )
-    }
+  state = {
+    count: 0
+  };
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h2 className={count > 10 ? "warning" : null}>Numbers: {count}</h2>
+        <h1>Hello World!</h1>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
+        >
+          -
+        </button>
+        {count > 10 ? (
+          <React.Suspense fallback={null}>
+            <Warning />
+          </React.Suspense>
+        ) : null}
+      </div>
+    );
+  }
 }
 
-export default hot(module)(App)
+export default hot(module)(App);
